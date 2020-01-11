66e92ba0f2457432eaca4f959fe7a6b0a32de9af
Matt Granmoe <mattgranmoe@gmail.com>
2020-01-11 12:34:56 -0600

    Attempt to get eslint fully working

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'react-app',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  // rules: {},
+}
